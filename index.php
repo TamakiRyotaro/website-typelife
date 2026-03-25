7dba458fbae0f8b1aae5084432f2174124ac3321
TamakiRyotaro <t.ryotaro.soccer.24@gmail.com>
2026-03-26 02:21:37 +0900

    ヘッダー悪戦苦闘

---
+++ index.php
@@ -0,0 +1,38 @@
+<!-- https://typlf.com/ -->
+ <!-- 学校用：http://localhost:8080/website-typelife/index.php -->
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/destyle.css@4.0.1/destyle.min.css">
+    <link rel="stylesheet" href="./style.css">
+    <title>Type Life</title>
+</head>
+<body>
+    
+    <header>
+        <a href="./index.php" class="header--topicon">
+            <div class="header-topicon--img"><img src="./img/icon.png" alt="TypeLifeのアイコン"></div>
+            <h1 class="header-topicon--title">TypeLife</h1>
+        </a>
+        <nav>
+            <ul class="header--navlist">
+                <li><a href="./index.php">about</a></li>
+                <li><a href="./business.php">business</a></li>
+                <li><a href="./recruit.php">recruit</a></li>
+                <li><a href="./contact.php">contact</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <main>
+        <p>System Engieering Service in Fukuoka</p>
+    </main>
+
+    <footer>
+        <small>&copy;<?php echo date("Y") ?> typelife</small>
+    </footer>
+
+</body>
+</html>
