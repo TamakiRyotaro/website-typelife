b0d8880244f5f12f5f2123bfb1b36d102ccf78cf
Tamaki Ryotaro <t.ryotaro.soccer.24@gmail.com>
2026-04-02 17:23:48 +0900

    aboutページ完成

--- page-about.php
+++ page-about.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 
-<main>
+<main class="subpage-main">
     <?php if (have_posts()): ?>
         <?php while (have_posts()): the_post(); ?>
 
@@ -15,23 +15,26 @@
                 </div>
 
                 <div class="about-content box-white">
+
                     <div class="about-image">
                         <?php echo wp_get_attachment_image(SCF::get("about-image"), "large"); ?>
                     </div>
                     <div class="about-text">
                         <?php the_content(); ?>
                     </div>
-                    <div class="about-list">
-                        <?php $company_info_group = SCF::get("about-list", get_page_by_path("about")->ID); ?>
-                        <?php foreach ($company_info_group as $fields): ?>
-                            <dl>
-                                <dt><?php echo $fields["about-list--title"] ?></dt>
-                                <dd><?php echo $fields["about-list--desc"] ?></dd>
-                            </dl>
-                        <?php endforeach; ?>
-                    </div>
-                    <div class="about-map">
-                        <?php echo SCF::get("about-map"); ?>
+                    <div class="about-flex">
+                        <div class="about-list">
+                            <?php $company_info_group = SCF::get("about-list", get_page_by_path("about")->ID); ?>
+                            <?php foreach ($company_info_group as $fields): ?>
+                                <dl>
+                                    <dt><?php echo $fields["about-list--title"] ?></dt>
+                                    <dd><?php echo $fields["about-list--desc"] ?></dd>
+                                </dl>
+                            <?php endforeach; ?>
+                        </div>
+                        <div class="about-map">
+                            <?php echo SCF::get("about-map"); ?>
+                        </div>
                     </div>
                 </div>
             </div>
--- page-business.php
+++ page-business.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 
-<main>
+<main class="subpage-main">
     <?php if (have_posts()): ?>
         <?php while (have_posts()): the_post(); ?>
 
@@ -13,11 +13,25 @@
                         <?php if (function_exists("bcn_display")) bcn_display(); ?>
                     </div>
                 </div>
+                <div class="business-content box-white">
 
-
-            </div>
-        <?php endwhile; ?>
-    <?php endif ?>
+                    <div class="business-image">
+                        <?php echo wp_get_attachment_image(SCF::get("business-image"), "large"); ?>
+                    </div>
+                    <div class="business-text">
+                        <?php the_content(); ?>
+                    </div>
+                    <div class="business-list">
+                        <?php $business_partner_group = SCF::get("business-partner", get_page_by_path("business")->ID); ?>
+                        <dl>
+                            <dt>主要取引先</dt>
+                            <?php foreach ($business_partner_group as $fields): ?>
+                                <dd><?php echo $fields["business-partner-name"] ?></dd>
+                            <?php endforeach; ?>
+                        </dl>
+                    </div>
+                <?php endwhile; ?>
+            <?php endif ?>
 </main>
 
 <?php get_footer(); ?>
